8a48b1695a1fb533d6286b0022abfca92e24933b
Ross Miller <rgmiller@ornl.gov>
2014-03-24 16:23:28 -0400

    New calculation function for beam monitor counts
    Added a new calculation function to get the beam monitor counts from the
    chunk workspace (which means we need to do the accumulation manually).
    Changed to original beam monitor function respond to pv names with the
    suffix _POST.  Also a few comment cleanups.

--- plugins/basic_variables.py
+++ plugins/basic_variables.py
@@ -6,22 +6,16 @@ Created on Mar 10, 2014
 Holds calculation functions for some of the more basic process variables
 '''
 
-
-
 # -----------------------------------------------------------------------------
 
 def calc_evtcnt( chunkWS, **extra_kwargs):
     '''
     Calculates the EVTCNT process variable.
     '''
-    
     # Note: We're operating on the chunkWS, which means we need to keep a
     # running sum of the events in a static variable and add the events
     # in chunkWS to it.  (And reset it to 0 when the run # changes.)
-    #
-    # Yes, this would be much easier to implement in the post-processing
-    # stage.  I'll work on that next.
-    
+        
     # The try...except paragraph initializes a couple of attributes on
     # the function.  This is the python equivalent of static variables
     try:
@@ -48,6 +42,7 @@ def calc_runnum( run_num, **extra_kwargs):
     
     # This one is just stupid simple...    
     return run_num   
+
 # -----------------------------------------------------------------------------
 
 def calc_protoncharge( chunkWS, **extra_kwargs):
@@ -81,22 +76,67 @@ def calc_protoncharge( chunkWS, **extra_kwargs):
     
     return calc_protoncharge.accum_charge
         
-# -----------------------------------------------------------------------------
-
 # -----------------------------------------------------------
-# HACK!!!
-# I want to try get the event counts from the post-processing step
-# This value should be the same as what's produced from the chunk
-# processing, but without the necessity of doing the accumulation myself
+
 def calc_evtcnt_post( accumWS, **extra_kwargs):
     '''
     Calculates the EVTCNT_POST process variable.
     '''
         
     return accumWS.getNumberEvents()
-# ------------------------------------------------
 
 # -----------------------------------------------------------
+
+class calc_beam_mon_cnt:
+    '''
+    Calculate values for beam monitor event count process variables
+    '''
+    
+    def __init__(self):
+        
+        # create all the attributes that the __call__() function is going to
+        # need
+        
+        # Since this class will work for multiple PV names, we use these
+        # dicts map a particular name to its associated value 
+        self._counts = {} # the accumulated beam monitor counts 
+        self._last_run_num = {} # The previous run number (so we know when to
+                                # reset the counts.
+                                
+    def __call__( self, chunkWS, pv_name, run_num, **kwargs):
+        # Sanity check pv_name
+        # Note: expects the name to be something like M1CNT, M2CNT, M99CNT, etc...
+        if pv_name[0] != 'M' or pv_name[-3:] != 'CNT':
+            # HACK!! What should we do here? Throw an exception?
+            return -1
+        
+        try:
+            int( pv_name[1:-3])
+        except ValueError:  # couldn't figure out the monitor number...
+            # HACK!! Again, should probably throw some other exception
+            return -1
+        
+        # initialize the counts and last run num, if necessary
+        if not pv_name in self._counts:
+            self._counts[pv_name] = 0;
+            self._last_run_num[pv_name] = run_num
+        
+        # do we need to reset the accumulated count?
+        if run_num != self._last_run_num[pv_name]:
+            self._counts[pv_name] = 0;
+            self._last_run_num[pv_name] = run_num
+        
+        
+        prop_name = "monitor" + pv_name[1:-3] + "_counts"
+        if chunkWS.run().hasProperty( prop_name):
+            prop = chunkWS.run().getProperty( prop_name)
+            self._counts[pv_name] += prop.value
+            return self._counts[pv_name]
+        else:
+            # HACK: should we throw an exception in this case?
+            return -1
+# -----------------------------------------------------------    
+
 def calc_beam_mon_cnt_post( accumWS, pv_name, **kwargs):
     '''
     Calculate values for beam monitor event count process variables
@@ -104,25 +144,26 @@ def calc_beam_mon_cnt_post( accumWS, pv_name, **kwargs):
     
     # Sanity check pv_name
     # Note: expects the name to be something like M1CNT, M2CNT, M99CNT, etc...
-    if pv_name[0] != 'M' or pv_name[-3:] != 'CNT':
+    if pv_name[0] != 'M' or pv_name[-8:] != 'CNT_POST':
         # HACK!! What should we do here? Throw an exception?
         return -1
     
     try:
-        int( pv_name[1:-3])
+        int( pv_name[1:-8])
     except ValueError:  # couldn't figure out the monitor number...
-        # HACK!! Again, should proably throw some other exception
+        # HACK!! Again, should probably throw some other exception
         return -1
     
-    prop_name = "monitor" + pv_name[1:-3] + "_counts"
+    prop_name = "monitor" + pv_name[1:-8] + "_counts"
     if accumWS.run().hasProperty( prop_name):
         prop = accumWS.run().getProperty( prop_name)
         return prop.value
     else:
         # HACK: should we throw an exception in this case?
         return -1
-# -----------------------------------------------------------    
 
+# -----------------------------------------------------------    
+# -----------------------------------------------------------
 
 def register_pvs():
     '''
@@ -140,7 +181,10 @@ def register_pvs():
     pv_functions_post[r'^EVTCNT_POST$'] = calc_evtcnt_post
     
     # should match M1CNT, M2CNT...M99CNT...M1001CNT, etc..
-    pv_functions_post[r'^M[0-9]+CNT$'] = calc_beam_mon_cnt_post
+    pv_functions_chunk[r'^M[0-9]+CNT$'] = calc_beam_mon_cnt()  # note that this is an instance of the class
+
+    # should match M1CNT, M2CNT_POST...M99CNT_POST...M1001CNT_POST, etc..
+    pv_functions_post[r'^M[0-9]+CNT_POST$'] = calc_beam_mon_cnt_post
     
     return (pv_functions_chunk, pv_functions_post)
     
