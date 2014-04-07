05c9f5989d28490e85358c7d929e63569e44bcf9
Ross Miller <rgmiller@ornl.gov>
2014-04-07 17:46:51 -0400

    More packaging work
    Changed the setup.py around to automatically generate its setup.cfg
    file.  This is because we need setup.cfg to specify the 'prefix'
    and install_lib options, but the setup.py script itself also needs to
    know these values.  It turns out the init.d script also needs to know
    these locations, so setup.py also reads a template for the init.d
    script and modifies it.  All this means that install directories only
    need to be set once in setup.py and the rpm will be built properly.
    
    Note: The init script (and also the startup script in the bin directory)
    still need some work.  The program doesn't actually start properly from
    the init script yet.

--- setup.py
+++ setup.py
@@ -5,6 +5,72 @@ Created on Mar 24, 2014
 '''
 
 from distutils.core import setup
+import sys
+import os
+import os.path
+
+# Install directories
+PREFIX  = "/opt/mantidstats"
+LIBDIR  = "%s/lib"%PREFIX
+BINDIR  = "%s/bin"%PREFIX
+CONFDIR = "%s/etc"%PREFIX
+INITDIR = "/etc/init.d"
+
+INIT_SCRIPT_NAME='mantidstats'
+
+# I want to specify some non-standard install directories and the easiest
+# way to do that is with a setup.cfg file.  Unfortunately, the values in
+# that file are not available to this script.  So, this function creates a
+# temporary setup.cfg file, puts the values I want in it, and closes the file.
+#
+# Yes, I know this is an abuse of how the setup.cfg file should be used.
+def create_temp_cfg_file():
+    f=open("setup.cfg", "w")
+    f.write("[install]\n")
+    f.write("prefix=%s\n"%PREFIX)
+    f.write("install_lib=%s\n"%LIBDIR)
+    #f.write("#install_scripts=some/bin/path\n"
+    #f.write("#install-base = /opt/mantidstats\n")
+    f.close()
+
+def remove_temp_cfg_file():
+    os.remove( 'setup.cfg')
+
+
+# Make sure the init.d script has the proper directories in it, too
+def generate_init_script():
+    infile = open('mantidstats.initd.template', 'r')
+    outfile = open(INIT_SCRIPT_NAME, 'w')
+    for l in infile.readlines():
+        # Look for strings to replace
+        n = l.find('__REPLACE_ME_EXEC__')
+        if n != -1:
+            outfile.write(l[0:n])
+            outfile.write("%s/mantidstats\n"%BINDIR)
+            continue
+        
+        n = l.find('__REPLACE_ME_CONFIGFILE__')
+        if n != -1:
+            outfile.write(l[0:n])
+            outfile.write("%s/mantidstats.conf\n"%CONFDIR)
+            continue
+        
+        outfile.write(l)
+        
+    infile.close()
+    outfile.close()
+    os.chmod( INIT_SCRIPT_NAME, 0755) # make the init.d script executable
+
+# Remove the updated init script once we're done with it
+def remove_init_script():
+    os.remove( INIT_SCRIPT_NAME)
+
+
+
+
+# -----------------------------------------------------------
+# The main part of the setup program
+# -----------------------------------------------------------
 
 # Describe the package in some detail
 long_desc = \
@@ -16,6 +82,27 @@ from data in the stream.
 Exactly which process variables are calculated is specified in an external
 configuration file. 
 '''
+
+# various non-python files
+data_files = [ (CONFDIR, ['mantidstats.conf']),
+               (INITDIR, [INIT_SCRIPT_NAME]),
+             ]
+
+# In the process of building the rpm package, this setup.py file actually gets
+# executed twice.  We only want to mess with the cfg and init.d files on the
+# first pass.
+if sys.argv[1] == 'bdist_rpm':
+    # Generate an init.d script with the proper directory names in it
+    generate_init_script()
+
+    # If a setup.cfg file exists, don't overwrite it.
+    # (We'll just have to hope that whoever put the file there
+    # knew what he/she was doing.)
+    delete_setup_cfg = False
+    print "----------", os.getcwd()
+    if not os.path.isfile( 'setup.cfg'):
+        delete_setup_cfg = True
+        create_temp_cfg_file()
 
 setup(name='MantidStatisticsServer',
       version='0.1Alpha',
@@ -33,13 +120,12 @@ setup(name='MantidStatisticsServer',
       package_dir = {'': 'lib'},
       packages = ['mantidstats','mantidstats.plugins'],
       scripts = ['bin/mantidstats'],
-      # data_files = [ ],
+      data_files = data_files,
      )
 
+if sys.argv[1] == 'bdist_rpm':
+    if delete_setup_cfg:
+        remove_temp_cfg_file()
 
+    remove_init_script()
 
-# An example of how the 'data_files' keyword works.  We'll probably have an
-# init.d script and a config file eventually.  They're not ready yet, though.
-#data_files=[('bitmaps', ['bm/b1.gif', 'bm/b2.gif']),
-#                  ('config', ['cfg/data.cfg']),
-#                  ('/etc/init.d', ['init-script'])
