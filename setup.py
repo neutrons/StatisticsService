7c014e1147ee065d66ec2a3b76649b2ffbd1dd19
Ross Miller <rgmiller@ornl.gov>
2014-05-15 10:36:05 -0400

    Bump the version number to 0.91Beta

--- setup.py
+++ setup.py
@@ -170,7 +170,7 @@ if sys.argv[1] == 'bdist_rpm':
         create_temp_cfg_file()
 
 setup(name='MantidStatisticsServer',
-      version='0.9Beta',
+      version='0.91Beta',
       description="A utility to generate EPICS PV's from the SNS ADARA stream",
       long_description= long_desc,
       author='Ross Miller',
